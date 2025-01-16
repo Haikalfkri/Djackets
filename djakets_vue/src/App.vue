10893e4f59c7119b0dd42d0b8a35e160ff5ef8cd
Haikal Fikri <haikaljr1245@gmail.com>
2025-01-16 16:10:47 +0700

    setup base template

--- djakets_vue/src/App.vue
+++ djakets_vue/src/App.vue
@@ -4,11 +4,39 @@
       <div class="navbar-brand">
         <router-link to="/" class="navbar-item"><strong>Djackets</strong></router-link>
 
-        <a href="" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu"></a>
+        <a href="" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-end">
+          <router-link to="/summer" class="navbar-item">Summer</router-link>
+          <router-link to="/winter" class="navbar-item">Winter</router-link>
+
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/log-in" class="button is-light">Log In</router-link>
+
+              <router-link to="/cart" class="button is-success">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Cart</span>
+              </router-link>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
 
-    <router-view/>
+    <section class="section">
+      <router-view/>
+    </section>
+
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2025</p>
+    </footer>
   </div>
 </template>
 
