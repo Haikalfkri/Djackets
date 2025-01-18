9ffd8f69b902ecf9323bf9cb9fa884df5d51e95a
Haikal Fikri <haikaljr1245@gmail.com>
2025-01-18 16:03:12 +0700

    create simple frontend and show product to the frontend

--- djakets_django/djakets_django/settings.py
+++ djakets_django/djakets_django/settings.py
@@ -46,20 +46,30 @@ INSTALLED_APPS = [
     "product",
 ]
 
-CORS_ALLOWED_ORIGIN = [
-    "http://localhost:8080",
-]
-
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4000",
+]
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
+]
+
 
 ROOT_URLCONF = "djakets_django.urls"
 
